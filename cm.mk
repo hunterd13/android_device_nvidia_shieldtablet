3e465f444e2054d85040f3ca4c94f275a205ade7
hunterd13 <res1gyxx@gmail.com>
2016-04-29 08:17:10 -0500

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, device/nvidia/shieldtablet/full_shieldtablet.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/liquid/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_shieldtablet
+PRODUCT_NAME := full_shieldtablet
 PRODUCT_DEVICE := shieldtablet

fbd4141d78e85a6c909f3858c064404be16d8f05
hunterd13 <res1gyxx@gmail.com>
2016-04-29 11:50:37 -0500

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -2,7 +2,13 @@
 $(call inherit-product, device/nvidia/shieldtablet/full_shieldtablet.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/liquid/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common.mk)
 
-PRODUCT_NAME := full_shieldtablet
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=shieldtablet \
+    TARGET_DEVICE=shieldtablet \
+    BUILD_FINGERPRINT="nvidia/shieldtablet/shieldtablet:6.0.1/:user/release-keys" \
+    PRIVATE_BUILD_DESC="shieldtablet-user 6.0.1 release-keys"
+
+PRODUCT_NAME := liquid_shieldtablet
 PRODUCT_DEVICE := shieldtablet
